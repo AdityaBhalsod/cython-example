51c3b395b3aed4abb2751df3db2a1f3d2f2c4dde
Aditya Bhalsod <adityabhalsod8@gmail.com>
2021-05-23 20:24:26 +0530

    Added commeting in .pyx file for better understading.

--- example_cython.pyx
+++ example_cython.pyx
@@ -1,6 +1,6 @@
-def test(int x):
-    cdef int y = 0
-    cdef int i
+cpdef test(int x):          # for cp is both cython and python usable.
+    cdef int y = 0          # for c is only cython usable.
+    cdef int i              # for c is only cython usable.
     for i in range(x):
         y += i
     return y
