d5b5a33445241b299b92303f4980db2a5cfd65e8
Aditya Bhalsod <adityabhalsod8@gmail.com>
2021-05-23 20:13:30 +0530

    Added example of cython.

---
+++ testing_things.py
@@ -0,0 +1,11 @@
+import timeit
+import multiprocessing
+
+cy = timeit.timeit('''example_cython.test(5000)''',setup='import example_cython',number=1)
+py = timeit.timeit('''example_original.test(5000)''',setup='import example_original', number=1)
+
+print(f"Total CPU: {multiprocessing.cpu_count()}")
+
+print(f"cython--> {cy}")
+print(f"python--> {py}")
+print('Cython is {}x faster'.format(py/cy))
